d9b0bc38eafbd30507ce29aef8a38eea41b14255
shankar santosh kenguva <sanker.dcm@gmail.com>
2016-12-21 10:57:29 +0530

    update

---
+++ employee-api12/target/classes/employee.sql
@@ -0,0 +1,8 @@
+create TABLE employee (
+  emp_id int(11) NOT NULL,
+  emp_name varchar(20),
+  emp_salary int(11),
+  email varchar(50),
+  status varchar(30),
+  PRIMARY KEY (emp_id)
+) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8;
